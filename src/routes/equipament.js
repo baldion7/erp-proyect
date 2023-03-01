d7ca9b777d66e946d2d4c354cb38ebaa65db7702
Kevin <baldionkevin8@gmail.com>
2023-03-01 10:03:59 -0500

    Terminacion de las rutas de get, post, put, delete funcionales y testiadas postman

--- src/routes/equipament.js
+++ src/routes/equipament.js
@@ -11,7 +11,38 @@ equiment
     .then((data)=>res.json(data))
     .catch((error)=>res.json({message:error}));
 });
+//lsit user
+router.get('/listuser',(req,res)=>{
+    equipmentSchema
+        .find()
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message:error}));
+});
+//listar usuario por a id
+router.get('/listuser/:id',(req,res)=>{
+    const {id}=req.params;
+    equipmentSchema
+        .findById(id)
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message:error}));
+});
+//update users
+router.put('/listuser/update/:id',(req,res)=>{
+    const {id}=req.params;
+    const {name}=req.body;
+    equipmentSchema
+        .updateOne({_id:id},{$set:{name}})
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message:error}));
+});
 
+router.delete('/listuser/delete/:id',(req,res)=>{
+    const {id}=req.params;
+    equipmentSchema
+        .remove(id)
+        .then((data)=>res.json(data))
+        .catch((error)=>res.json({message:error}));
+});
 
 
 module.exports=router;
